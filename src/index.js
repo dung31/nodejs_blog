1c85cd119d60daf1a0d3da28e9feb914e0bbe8ad
dung31 <79446796+dung31@users.noreply.github.com>
2022-07-27 18:27:01 +0700

    create routes , app/controller folder

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(morgan("combined"));
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -18,13 +20,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+//routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
